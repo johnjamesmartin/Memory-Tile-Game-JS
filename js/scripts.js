aaabf89d1f1f1eb9aa54f2f17ccee4e9bdf142f7
John Martin <johnjamesmartin@live.com>
2019-07-27 18:57:24 +0100

    Clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
+/* Variables:
+ ***************************************/
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+/* Flip card:
+ ***************************************/
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
 
@@ -21,35 +23,41 @@ function flipCard() {
   checkForMatch();
 }
 
+/* Check for a match:
+ ***************************************/
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
   isMatch ? disableCards() : unflipCards();
 }
 
+/* Disable cards:
+ ***************************************/
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
 }
 
+/* Unflip cards:
+ ***************************************/
 function unflipCards() {
   lockBoard = true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1500);
 }
 
+/* Reset board:
+ ***************************************/
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
 
+/* Shuffle cards:
+ ***************************************/
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
@@ -57,4 +65,6 @@ function resetBoard() {
   });
 })();
 
+/* Add listeners:
+ ***************************************/
 cards.forEach(card => card.addEventListener('click', flipCard));
